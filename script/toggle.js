58235b2921627a5ece8f93a2e9c00e1143ec9c08
shifat80 <mehedihasanshipat4@gmail.com>
2025-03-01 19:14:19 +0600

    completed all

--- script/toggle.js
+++ script/toggle.js
@@ -1,3 +1,7 @@
 document.getElementById("blog-btn").addEventListener("click",function(){
     window.location.href = "blog.html";
 });
+
+document.getElementById("back-btn").addEventListener("click",function(){
+    window.location.href = "index.html";
+});
